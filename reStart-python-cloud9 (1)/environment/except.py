0ff8f9a811856f1ba3a1a3521aac638fbc3ae656
Rafaela108 <123127201+Rafaela108@users.noreply.github.com>
2024-03-03 11:43:22 +0200

    Delete reStart-python-cloud9 (1)/environment directory

--- reStart-python-cloud9 (1)/environment/except.py
+++
@@ -1,9 +0,0 @@
-def divide_five_by(number):
-    try:
-        value = 5 / number
-    except ZeroDivisionError:
-        value = 1
-    return value
-print(divide_five_by(2))
-print(divide_five_by(0))
-         
